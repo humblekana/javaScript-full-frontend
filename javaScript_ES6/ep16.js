dc40089287fccb78a07b6beff06300ff11eb8b34
beta <lnbtkana2326@gmail.com>
2022-04-28 09:59:47 +0700

    commit

--- javaScript_ES6/ep16.js
+++ javaScript_ES6/ep16.js
@@ -10,14 +10,11 @@ console.log(filterData)
 
 //reduce array.reduce(()=>{},ค่าเริ่มต้น) value = 0
 const info = [10,20,30,40,50]
-let result =info.reduce((value,e)=>{
-    const total = e+value
-    return total
-},0)
+let result =info.reduce((value,e)=>value+=e,0)
 console.log(result)
 
 
-const cart = [{fname:"กระเป๋า",price:500},
+/*const cart = [{fname:"กระเป๋า",price:500},
     {fname:"หนังสือ",price:100},
     {fname:"ขนม",price:2000}
 ]
@@ -26,4 +23,4 @@ let resultcart = cart.reduce((value,e)=>{
     const total = e.price+value
     return total
 },0)
-console.log(resultcart)
+console.log(resultcart)*/
