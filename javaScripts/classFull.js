4b4b56969fa2c7021e00077f1932a262134dd518
beta <lnbtkana2326@gmail.com>
2022-05-10 00:28:58 +0700

    commit

---
+++ javaScripts/classFull.js
@@ -0,0 +1,49 @@
+// class other shape
+class Shape {
+    constructor(num){
+        this.num = num
+    }
+    rectangle(){
+        for (let i=0;i<this.num;i++){
+            let str = ""
+            for (let j=0;j<this.num;j++){
+                str += "* "
+            }
+            console.log(str)
+        }
+    }
+    triangle(){
+        for (let i=0;i<this.num;i++){
+            let str = ""
+            for (let j=0;j<i+1;j++){
+                str += "* "
+            }
+            console.log(str)
+        }
+    }
+}
+
+// class Date
+class Displayinfo{
+    static displayName(){
+        let fname = "Sukachathum"
+        let sname = "Seawsiritaworn"
+        console.log(fname,sname)
+    }
+    static displayAge(){
+        console.log(`${this.sendAge()}`)    
+    }
+    
+    static sendAge(){
+        let age = Math.floor(Math.random()*50)
+        return age
+    }
+    
+    defraud(){
+        console.log(Displayinfo.sendAge())
+    }
+
+}
+
+
+
